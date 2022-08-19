c6f1d17b1d24c377ec22be62cd8b522a56de5a1b
Fabien RIBES <fabien.ribes@eove.fr>
2022-08-19 18:00:44 +0200

    add gui

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
+use eframe::egui;
 mod fronius;
 mod utils;
 
 fn main() {
-  let content = fronius::get();
-  let powers = fronius::retrieve_power_measure(&content);
-  println!("Solar power : {}", utils::format_generic_power(powers.p_pv));
-  println!("Load power : {}", utils::format_generic_power(powers.p_load));
-  println!("Balance to grid : {}", utils::format_grid_power(powers.p_grid));
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "egui app",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
 }
+
+struct MyApp {
+    powers: fronius::Power
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            powers: fronius::Power { p_grid: 0.0, p_load: 0.0, p_pv: 0.0 }
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Rust egui");
+            if ui.button("Update").clicked() {
+                let content = fronius::get();
+                self.powers = fronius::retrieve_power_measure(&content);
+            }
+            ui.label(format!("Solar power : {}", utils::format_generic_power(self.powers.p_pv)));
+            ui.label(format!("Load power : {}", utils::format_generic_power(self.powers.p_load)));
+            ui.label(format!("Balance to grid : {}", utils::format_grid_power(self.powers.p_grid)));
+        });
+    }
+}
+
